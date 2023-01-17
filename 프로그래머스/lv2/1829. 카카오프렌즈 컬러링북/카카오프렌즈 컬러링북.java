08a37dce4239f990b04d8aef444a33aa03aaf0d0
suuumim <83461362+tnals2384@users.noreply.github.com>
2023-01-17 20:26:27 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 23.88 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -1,46 +1,61 @@
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Queue;
 
-class Solution {
-     int[] dx ={1,-1,0,0};
-    int[] dy ={0,0,1,-1};
+public class Solution {
+    int[] dx = {1,-1,0,0};
+    int[] dy = {0,0,1,-1};
     int count=0;
 
-    public int[] solution(int m, int n, int[][] picture) {
+    public int[] solution(int m, int n , int[][] picture) {
         int numberOfArea = 0;
         int maxSizeOfOneArea = 0;
-        List<Integer> size = new LinkedList<>();
         boolean[][] check = new boolean[m][n];
 
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
                 if(picture[i][j]!=0 && !check[i][j]) {
                     numberOfArea++;
-                    dfs(i, j, check, picture);
-                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, count);
-                    count = 0;
+                    bfs(i,j,picture,check);
+                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea,count);
+                    count=0;
                 }
             }
         }
-           
         int[] answer = new int[2];
         answer[0] = numberOfArea;
         answer[1] = maxSizeOfOneArea;
         return answer;
     }
 
-    public void dfs(int x,int y,boolean[][] check,int [][] picture) {
-       
+    public class Point {
+        int x;
+        int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public void bfs(int x,int y, int[][] picture, boolean[][] check) {
+        Queue<Point> queue = new LinkedList<>();
         check[x][y]=true;
         count++;
-        for(int i=0; i<4;i++) {
-            int X = x + dx[i];
-            int Y = y + dy[i];
-            if( X<0 || X >= picture.length  || Y < 0 || Y >= picture[0].length) continue;
+        queue.offer(new Point(x,y));
 
-            if(picture[X][Y]==picture[x][y] && !check[X][Y]) {
-                dfs(X,Y,check,picture);
+        while(!queue.isEmpty()) {
+            Point q=queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int X = q.x + dx[i];
+                int Y = q.y + dy[i];
+                if (X >= 0 && X < picture.length && Y >= 0 && Y < picture[0].length
+                        && !check[X][Y] && picture[X][Y] == picture[x][y]) {
+                    queue.offer(new Point(X, Y));
+                    check[X][Y] = true;
+                    count++;
+                }
             }
         }
     }
 }
+
