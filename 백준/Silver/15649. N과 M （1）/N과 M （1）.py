c2f6b8dca6fbe63eca58f61f778c2d2ff47e046b
suuumim <83461362+tnals2384@users.noreply.github.com>
2023-04-12 16:16:48 +0900

    Update N과 M （1）.py
    backtracking이용

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,3 +1,31 @@
+# 백트래킹 이용하여 풀기
+N,M= map(int, input().split())
+answer= []
+visited = [False]*(N+1)
+
+def backtracking():
+    #M개가 되면 출력
+    if len(answer) == M:
+        print(*answer)
+        return
+    
+    for i in range(1,N+1):
+        #방문 했으면 건너뛰기
+        if visited[i] == True :
+            continue
+        #방문 안했을 경우 answer에 추가하고, 함수 다시 실행
+        visited[i]=True
+        answer.append(i)
+        backtracking()
+        #다음 i를 위해, 추가한 거 다시 빼주고, 방문도 False로 바꿔두기
+        answer.pop()
+        visited[i]=False
+    
+    
+backtracking()
+
+#-----------------------------------------------
+# 파이썬 itertools permutations 이용하여 풀기
 import itertools
 
 N,M= map(int, input().split())
