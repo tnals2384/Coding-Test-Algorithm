3e298835cb2186ef8cc29855cded1b7ec7831a7e
suuumim <83461362+tnals2384@users.noreply.github.com>
2023-05-25 20:42:21 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,23 +1,13 @@
-height = []
-for _ in range(9):
-    height.append(int(input()))
+from itertools import combinations
 
+height = [int(input()) for _ in range(9)]
 
-sum=sum(height)
-remove1=0
-remove2=0
+li = list(combinations(height,7))
 
-for i in range(9):
-    for j in range(i+1,9):
-        if sum-height[i]-height[j]==100:
-            remove1,remove2=height[i],height[j]
-            break
-
-        
-height.remove(remove1)
-height.remove(remove2)
-height.sort()
-
-for i in range(7):
-    print(height[i])
-        
+for i in li :
+    if sum(i)==100:
+        answer=list(i)
+        break
+answer.sort()
+for x in answer:
+    print(x)
