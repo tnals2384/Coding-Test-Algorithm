7bc0eb74af2cf51d806d7ca4b1384d43633c3983
suuumim <83461362+tnals2384@users.noreply.github.com>
2023-07-19 13:37:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 348 ms, Memory: 49376 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -2,6 +2,7 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
+from collections import deque
 N= int(input())
 
 tree = [[] for i in range(N+1)]
@@ -12,12 +13,24 @@ for i in range(N-1):
     tree[y].append(x)
 
 
+#dfs 풀이
 def dfs(tree,v, answer) :
     for i in tree[v] :
         if answer[i] == 0 :
             answer[i]=v
             dfs(tree,i,answer)
+# bfs 풀이       
+def bfs(tree, v, answer) :
+    q = deque([v])
+    answer[v]=1
+    while q:
+        x= q.popleft()
+        for i in tree[x]:
+            if answer[i]==0:
+                q.append(i)
+                answer[i]=x
         
-dfs(tree,1,answer)
+#dfs(tree,1,answer)
+bfs(tree,1,answer)
 for i in range(2,N+1):
     print(answer[i])
