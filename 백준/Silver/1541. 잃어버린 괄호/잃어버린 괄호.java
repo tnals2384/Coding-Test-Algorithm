d627e012f4d4305b32098905b6ba5f8288b319ab
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-01-08 14:30:29 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,26 +3,29 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws  Exception{
-        
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+        //먼저 -로 split
         String[] token = br.readLine().split("-");
-        int[] num= new int[token.length];
-        
+        int[] list = new int[token.length];
+
         for(int i=0;i<token.length;i++) {
+            //plus로 split 후 더할 수 있는 건 다 더해서 list에 저장
             String[] plus = token[i].split("\\+");
-            int count=0;
-            
-            for(int j=0;j< plus.length;j++) {
-                count+=Integer.parseInt(plus[j]);
+            int num=0;
+            for(int j=0;j<plus.length;j++) {
+                num += Integer.parseInt(plus[j]);
             }
-            
-            num[i]=count;
+            list[i]=num;
         }
 
-        int min = num[0];
-        for(int i=1;i<token.length;i++)
-            min-=num[i];
+        //list에 있는 수는 다 빼기
+        int ans=list[0];
+        for (int i=1;i<list.length;i++) {
+            ans-=list[i];
+        }
 
-        System.out.println(min);
+        System.out.println(ans);
     }
 }
+
