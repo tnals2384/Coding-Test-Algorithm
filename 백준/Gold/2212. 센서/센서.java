5afe6b9661902014eb5d3ef610691804c4fa6bc6
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-01-12 13:34:57 +0900

    [Gold V] Title: 센서, Time: 428 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -11,20 +11,23 @@ public class Main {
         List<Integer> sensor = new ArrayList<>();
         int[] minus = new int[N-1];
         int count=0;
+       
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++) {
             int x = Integer.parseInt(st.nextToken());
+            //중복을 제거해야함
             if (sensor.contains(x))
                     continue;
             sensor.add(x);
         }
 
+        //정렬 후 거리 차 구하기
         Collections.sort(sensor);
         for (int i=1;i<sensor.size();i++) {
             minus[i-1]=sensor.get(i) - sensor.get(i-1);
         }
 
-
+        //거리차를 정렬 한 후, K-1개의 큰 값을 빼고 더함
         Arrays.sort(minus);
         for (int i=0; i<minus.length-(K-1);i++)
             count+=minus[i];
