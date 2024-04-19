aca4b4a107109cb71a579d263ec4ba7898277ccf
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-04-19 21:17:06 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,17 +2,18 @@ class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder();
         int index = 0;
-        int max = 0;
-        for(int i = 0; i<number.length() - k; i++) {
-            max = 0;
-            for(int j = index; j<= k+i; j++) {
-                if(max < number.charAt(j)-'0') {
+        //총 넘버개수중에 k개를 빼야됨
+        for(int i=0;i < number.length() - k;i++) {
+            int max = 0;
+            for(int j=index;j<=k+i;j++) {
+                if(number.charAt(j) -'0' > max) {
                     max = number.charAt(j)-'0';
-                    index = j + 1;
+                    index = j+1;
                 }
             }
             sb.append(max);
         }
+        
         return sb.toString();
     }
 }
