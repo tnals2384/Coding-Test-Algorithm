cfe2ff0e782f5f41275106380d2409fa146d169c
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-04-23 11:03:37 +0900

    [level 2] Title: 프로세스, Time: 0.57 ms, Memory: 76.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<priorities.length;i++) 
+            queue.add(priorities[i]);
+        
+        while(!queue.isEmpty()) {
+            for(int i=0;i<priorities.length;i++) {
+                if(queue.peek() == priorities[i])  {
+                    queue.poll();
+                    answer++;
+                    if(location == i)
+                    return answer;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
