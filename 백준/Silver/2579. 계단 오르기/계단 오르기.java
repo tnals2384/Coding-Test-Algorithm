69288f6e4a9a972d9673c3601d48cb80b7a685c1
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-05-08 00:23:03 +0900

    [Silver III] Title: 계단 오르기, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,24 +1,26 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] score = new int[N + 1];
-        int[] dp = new int[N + 1];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N+1];
+        int[] dp = new int[N+1];
+        for(int i=1;i<=N;i++)
+            A[i]= Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            score[i] = sc.nextInt();
-        }
-
-        dp[1]=score[1];
+        dp[1]=A[1];
         if(N>=2)
-            dp[2]=score[1]+score[2];
+            dp[2]=A[1]+A[2];
 
-        for(int i=3;i<N+1;i++) {
-            dp[i] = Math.max(dp[i-2], dp[i-3]+score[i-1])+score[i];
+        for(int i=3;i<=N;i++) {
+            dp[i]= Math.max(dp[i-2], dp[i-3]+A[i-1])+A[i];
         }
 
         System.out.println(dp[N]);
+
+
     }
 }
