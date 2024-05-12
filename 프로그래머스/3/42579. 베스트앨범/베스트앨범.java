ff1b12d282b6d05d0a2c0129e160899602c719ee
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-05-12 19:10:18 +0900

    [level 3] Title: 베스트앨범, Time: 4.06 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,36 +2,36 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
-        HashMap<String, Integer> total = new HashMap<>();
-        HashMap<String, HashMap<Integer, Integer>> inGenre = new HashMap<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<String,Integer> total = new HashMap<>();
+        HashMap<String,HashMap<Integer,Integer>> genre = new HashMap<>();
 
-        for(int i=0;i < genres.length;i++) {
+        for(int i = 0;i < genres.length; i++) {
             if(!total.containsKey(genres[i])) {
+                total.put(genres[i], plays[i]);
                 HashMap<Integer, Integer> map = new HashMap<>();
                 map.put(i, plays[i]);
-                inGenre.put(genres[i], map);
-                total.put(genres[i], plays[i]);
-            } else {
-                inGenre.get(genres[i]).put(i, plays[i]);
-                total.put(genres[i], total.get(genres[i]) + plays[i]);
+                genre.put(genres[i], map);
+            }
+            else {
+                total.put(genres[i],total.get(genres[i])+plays[i]);
+                genre.get(genres[i]).put(i,plays[i]);
             }
         }
 
-        List<String> keySet = new ArrayList<>(total.keySet());
-        keySet.sort(Comparator.comparing(total::get).reversed());
+        ArrayList<String> keys = new ArrayList<>(total.keySet());
+        keys.sort(Comparator.comparingInt(total::get).reversed());
 
-
-        for(String key : keySet) {
-            HashMap<Integer,Integer> map = inGenre.get(key);
-            List<Integer> genreKeys = new ArrayList<>(map.keySet());
-            genreKeys.sort(Comparator.comparing(map::get).reversed());
- 
-            answer.add(genreKeys.get(0));
-            if(genreKeys.size() > 1)
-                answer.add(genreKeys.get(1));
+        for(String key : keys) {
+            ArrayList<Integer> indexs = new ArrayList<>(genre.get(key).keySet());
+            indexs.sort(Comparator.comparingInt(genre.get(key)::get).reversed());
+            for(int i=0;i<2;i++) {
+                answer.add(indexs.get(i));
+                if(indexs.size() < 2)
+                    break;
+            }
         }
-        
+
         return answer.stream().mapToInt(i->i).toArray();
     }
 }
