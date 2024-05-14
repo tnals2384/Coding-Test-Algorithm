ec614c2c46b48fc9e103e210b5b56703b11d8f78
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-05-14 14:41:11 +0900

    [D3] Title: 햄버거 다이어트, Time: 261 ms, Memory: 46,184 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,47 +1,13 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
+import java.time.Year;
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-    static int max = 0;
+public class Solution {
     static int L;
     static int N;
-    static boolean[] visited;
     static int[] score;
     static int[] cal;
-	public static void main(String args[]) throws Exception
-	{
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int T = sc.nextInt();
@@ -49,35 +15,30 @@ class Solution
         for (int test_case = 1; test_case <= T; test_case++) {
             N = sc.nextInt();
             L = sc.nextInt();
-            score = new int[N];
-            cal = new int[N];
-            visited = new boolean[N];
-            max=0;
-            for (int i = 0; i < N; i++) {
+            score = new int[N+1];
+            cal = new int[N+1];
+            int[][] dp  = new int[N+1][L+1];
+            for(int i =1 ;i<=N;i++) {
                 score[i] = sc.nextInt();
                 cal[i] = sc.nextInt();
             }
 
-            dfs(0,0,0);
-
-            System.out.println("#"+test_case+" "+max);
-        }
-
-    }
-
-    public static void dfs(int start, int calSum, int scoreSum) {
-        if (calSum <= L)
-            max = Math.max(max, scoreSum);
-
-        if (start == N)
-            return;
-
-        for (int i = start; i < N; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(i + 1, calSum + cal[i], scoreSum + score[i]);
-                visited[i] = false;
+            for(int i=1;i<=N;i++) {
+                for (int j = 1; j <= L; j++) {
+                    //i번째 무게를 더 담을 수 없는 경우, 전 꺼 그대로
+                    if (cal[i] > j) {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                    //i번째 무게를 더 담을 수 있는 경우
+                    //전 꺼랑, (무게가 j-cal[i]일때 가치 + 지금 가치) 를 비교해서 더 큰 것을 선택
+                    else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cal[i]] + score[i]);
+                    }
+                }
             }
+
+            System.out.println("#"+test_case+" "+dp[N][L]);
         }
+
     }
 }
