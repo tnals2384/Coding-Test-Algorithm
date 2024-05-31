7c5bd8aa7cd534dff0238c15524700e2a53f6ace
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-06-01 02:59:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.79 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,42 +2,43 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
+        
+        //최대 힙, 최소 힙 선언
+        PriorityQueue<Integer> max = new PriorityQueue<>(Comparator.reverseOrder());
         PriorityQueue<Integer> min = new PriorityQueue<>();
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());  
-        List<Integer> visited = new ArrayList<>();
-        for(String operation: operations) {
-            String[] st = operation.split(" ");
-            String cmd = st[0];
-            Integer num = Integer.parseInt(st[1]);
-            if(cmd.equals("I")) {
-                min.add(num);
+
+        for (String operation : operations) {
+            String[] op = operation.split(" ");
+            int num = Integer.parseInt(op[1]);
+            
+            //커맨드가 I이면 큐에 삽입
+            if (op[0].equals("I")) {
                 max.add(num);
-            }
-            else {
-                if(num == -1) {
-                    if(!min.isEmpty()) {
+                min.add(num);
+            } else { //커맨드가 D이고
+                //숫자가 1이면 최대값 삭제
+                if (op[1].equals("1")) {
+                    if (!max.isEmpty()) {
+                        int n = max.poll();
+                        min.remove(n);
+                    }
+                } else { //숫자가 -1이면 최소값 삭제
+                    if (!min.isEmpty()) {
                         int n = min.poll();
                         max.remove(n);
                     }
                 }
-                else {
-                    if(!max.isEmpty()) {
-                        int n = max.poll();
-                        min.remove(n);
-                    }
-                }
             }
         }
-        if(min.isEmpty() || max.isEmpty()) {
-            answer[0]=0;
-            answer[1]=0;
-            return answer;
-        }
         
+        //큐가 비어있으면 0,0 출력
+        if(min.isEmpty() || max.isEmpty()){
+            return new int[] {0,0};
+        }
+
         answer[0] = max.poll();
         answer[1] = min.poll();
-        
-        
+
         return answer;
     }
 }
