8e466fb63afd47e0256481aa07f073df7d83d93d
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-06-10 12:58:36 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.10 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,29 +1,25 @@
 class Solution
 {
-    public int solution(String s)
-    {
+    public int solution(String s) {
         int answer = 1;
-        
-        for(int i=s.length(); i > 0; i--) {
-            for(int j=0; j + i < s.length(); j++) {
-                int start = j;
-                int end = j + i;
-                boolean flag = false;
-                
+
+        for(int i = s.length()-1; i > 0 ; i--) {
+            for(int j = 0; j+i < s.length(); j++) {
+                int start = j; 
+                int end = i + j;
+              
                 while(start < end) {
-                    if(s.charAt(start) != s.charAt(end)) {
-                        flag = true;
-                        break;
+                    if(s.charAt(start) == s.charAt(end)) {
+                        start++;
+                        end--;
                     }
-                    start++;
-                    end--;
+                    else break;
                 }
                 
-                if(!flag)
+                if(start >= end)
                     return i+1;
             }
         }
-
         return answer;
     }
 }
