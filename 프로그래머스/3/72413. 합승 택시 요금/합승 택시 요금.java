2858de03f867a547c604e2d2d7c1480741d7f6d8
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-06-13 11:37:31 +0900

    [level 3] Title: 합승 택시 요금, Time: 3.27 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,6 +1,71 @@
 import java.util.*;
 
 class Solution {
+    ArrayList<Node>[] graph;
+    public class Node {
+        int x;
+        int w;
+        
+        public Node(int x, int w) {
+            this.x = x;
+            this.w = w;
+        }
+    }
+    //다익스트라
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+         int answer = Integer.MAX_VALUE;
+         
+         graph = new ArrayList[n+1];
+         for(int i=1;i<=n;i++) {
+             graph[i] = new ArrayList<>();
+         }
+         
+         for(int[] fare: fares) {
+             graph[fare[0]].add(new Node(fare[1],fare[2]));
+             graph[fare[1]].add(new Node(fare[0],fare[2]));
+         }
+        
+        int[] stok = dijkstra(s,n);
+        int[] atok = dijkstra(a,n);
+        int[] btok = dijkstra(b,n);
+        
+        for(int i=1;i<=n;i++) {
+            answer= Math.min(answer, stok[i]+atok[i]+btok[i]);
+        }
+        
+         return answer;
+    }
+    
+   public int[] dijkstra(int start, int n) {
+        boolean[] visited = new boolean[n+1];
+        int[] cost = new int[n+1];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+       
+        cost[start]=0;
+       
+        PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2) -> o1.w-o2.w);
+       
+        queue.add(new Node(start, 0));
+       
+        while(!queue.isEmpty()) {
+           Node now = queue.poll();
+           
+           if(!visited[now.x]) {
+               visited[now.x] = true;
+               
+               for(Node node : graph[now.x]) {
+                   if(!visited[node.x] && cost[node.x] > cost[now.x] + node.w) {
+                       cost[node.x] = cost[now.x] + node.w;
+                       queue.add(new Node(node.x, cost[node.x]));
+                   }
+               }
+           }
+       }
+       
+       return cost;
+   }
+    
+    /* 플로이드 워셜
     public int solution(int n, int s, int a, int b, int[][] fares) {
         int answer = Integer.MAX_VALUE;
         
@@ -30,4 +95,5 @@ class Solution {
         }
         return answer;
     }
+    */
 }
