0961a5a0cb443cae9cfc46dfe489c9db37f6bbcf
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-06-27 00:12:31 +0900

    [level 2] Title: 땅따먹기, Time: 17.65 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,16 +1,15 @@
 import java.util.*;
 
 class Solution {
-    int n;
-    int max=0;
+
     int solution(int[][] land) {
-        n = land.length;
+        int n = land.length;
         int[][] dp = new int[n][4];
-        int max= 0;
+
         dp[0] = land[0];
         for(int i = 1; i < n; i++) {
-            for(int j=0; j<4; j++) {
-                for(int k=0; k<4; k++) {
+            for(int j=0; j < 4; j++) {
+                for(int k=0; k < 4; k++) {
                     if(j==k) continue;
                     dp[i][j] = Math.max(dp[i][j], land[i][j] + dp[i-1][k]);
                 }
