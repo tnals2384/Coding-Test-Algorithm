dba366c242ad109d622d9fe626557a9d7cbd49ba
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-07-18 14:28:26 +0900

    [level 2] Title: 주식가격, Time: 40.12 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,17 +2,24 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack =new Stack<>();
-        for(int i=0;i<prices.length;i++)
-            answer[i] = prices.length-i-1;
+        Stack<Integer> stack = new Stack<>();
         
-        for(int i=0;i<prices.length;i++) {
+        //1. answer를 초기화하기
+        for(int i=0; i < prices.length; i++)
+            answer[i] = prices.length-1-i;
+    
+        //2. stack에 index를 넣음
+        for(int i = 0; i < prices.length; i++) {
+            
+            //3. 스택에 prices[i]보다 큰 수의 인덱스가 있는 동안 반복
             while(!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                answer[stack.peek()] = i-stack.peek();
+                answer[stack.peek()] = i - stack.peek();
                 stack.pop();
             }
+            //4. stack에 넣기
             stack.push(i);
         }
+        
         return answer;
     }
 }
