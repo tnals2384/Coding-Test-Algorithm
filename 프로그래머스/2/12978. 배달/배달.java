8ae9d710b7798ab06cc73c3e99189f6b2c6c1c99
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-08-09 11:18:25 +0900

    [level 2] Title: 배달, Time: 14.23 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,49 +1,56 @@
 import java.util.*;
-
 class Solution {
-    public class Node {
+    
+    class Node {
         int x;
-        int w;
+        int c;
         
-        public Node(int x, int w) {
+        public Node(int x, int c) {
             this.x = x;
-            this.w = w;
+            this.c = c;
         }
     }
+    
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-        ArrayList<Node> [] graph = new ArrayList [N+1];
-        for(int i=1;i<=N;i++) {
-            graph[i]= new ArrayList<Node>();
-        }
+ 
+        //인접 리스트 활용
+        ArrayList<Node>[] graph = new ArrayList[N+1];
+        for(int i=0;i<=N;i++)
+            graph[i] = new ArrayList<>();
+        
+        int[] time = new int[N+1];
+        Arrays.fill(time, Integer.MAX_VALUE);
+        
+        time[1] = 0;
         
         for(int[] r : road) {
             graph[r[0]].add(new Node(r[1],r[2]));
             graph[r[1]].add(new Node(r[0],r[2]));
         }
         
+        PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2) -> o1.c - o2.c);
         boolean[] visited = new boolean[N+1];
-        //time
-        int[] time = new int[N+1];
-        Arrays.fill(time,Integer.MAX_VALUE);
-        
-        time[1] = 0;
-        
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1,o2) -> o1.w - o2.w);
         queue.add(new Node(1,0));
         
+        //탐색 시작
         while(!queue.isEmpty()) {
             Node now = queue.poll();
             if(visited[now.x]) continue;
             visited[now.x] = true;
-            for(Node n: graph[now.x]) {
-                time[n.x] = Math.min(time[n.x], time[now.x] + n.w);
+            
+            for(Node n : graph[now.x]) {
+                if(visited[n.x]) continue;
+                if(time[n.x] > time[now.x] + n.c)
+                    time[n.x] = time[now.x] + n.c;
+                
                 queue.add(new Node(n.x, time[n.x]));
             }
         }
         
-        for(int i=1;i<=N;i++)
-            if(time[i]<=K) answer++;
+        for(int i=0;i<=N;i++) {
+            if(time[i] <= K) answer++;
+        }
         return answer;
     }
 }
