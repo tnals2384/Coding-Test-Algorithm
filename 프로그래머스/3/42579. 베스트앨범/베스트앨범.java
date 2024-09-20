683ea3f03f3e9d330c2f1054523f8c4aa53c9c30
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-09-20 15:47:27 +0900

    [level 3] Title: 베스트앨범, Time: 6.38 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,37 +1,35 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<String,Integer> total = new HashMap<>();
-        HashMap<String,HashMap<Integer,Integer>> genre = new HashMap<>();
-
-        for(int i = 0;i < genres.length; i++) {
+        HashMap<String, Integer> total = new HashMap<>();
+        HashMap<String, Map<Integer, Integer>> genre = new HashMap<>();
+        
+        for(int i=0;i<genres.length;i++) {
             if(!total.containsKey(genres[i])) {
                 total.put(genres[i], plays[i]);
                 HashMap<Integer, Integer> map = new HashMap<>();
                 map.put(i, plays[i]);
-                genre.put(genres[i], map);
+                genre.put(genres[i],map);
             }
             else {
-                total.put(genres[i],total.get(genres[i])+plays[i]);
+                total.put(genres[i], total.get(genres[i])+plays[i]);
                 genre.get(genres[i]).put(i,plays[i]);
             }
         }
-
+        
         ArrayList<String> keys = new ArrayList<>(total.keySet());
-        keys.sort(Comparator.comparingInt(total::get).reversed());
-
+        keys.sort(Comparator.comparingInt(i -> total.get(i)).reversed());
+        
         for(String key : keys) {
-            ArrayList<Integer> indexs = new ArrayList<>(genre.get(key).keySet());
-            indexs.sort(Comparator.comparingInt(genre.get(key)::get).reversed());
+            ArrayList<Integer> genreKeys = new ArrayList<>(genre.get(key).keySet());
+            genreKeys.sort(Comparator.comparingInt(i -> genre.get(key).get(i)).reversed());
+            
             for(int i=0;i<2;i++) {
-                answer.add(indexs.get(i));
-                if(indexs.size() < 2)
-                    break;
+                if(genreKeys.size() > i)
+                    answer.add(genreKeys.get(i));
             }
         }
-
-        return answer.stream().mapToInt(i->i).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
