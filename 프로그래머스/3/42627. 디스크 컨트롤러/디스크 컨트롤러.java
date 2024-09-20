aad55de679e5a869a6826df007acda6ab6e4e58f
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-09-20 17:38:45 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.14 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,36 +1,31 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
         
-        //작업 시작 가능한 것들 중 소요시간이 짧은 순으로 정렬
-        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparing((int[] o) -> o[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2) -> o1[1] - o2[1]);
         
+        int N = 0;
+        int idx = -1;
         int now = 0;
-        int count = 0;
-        int start = -1;
-        
-        while(count < jobs.length) {
-           
-            //요청 시간이 전 작업 시작시간보다 크고, 현재 시간보다 작거나 같으면 시작 가능한 작업
+        while(N < jobs.length) {
             for(int[] job : jobs) {
-                if(start < job[0] && now >= job[0]) {
-                    queue.add(job);
+                if(idx < job[0] && job[0] <= now) {
+                    pq.add(job);
                 }
             }
             
-            //시작 가능한 작업이 있으면 시작해주고 시간 더하기
-            if(!queue.isEmpty()) {
-                int[] job = queue.poll();
-                start = now;
+            if(!pq.isEmpty()) {
+                int[] job = pq.poll(); 
+                idx = now;
                 now += job[1];
                 answer += now - job[0];
-                count++;
+                N++;
             }
-            //시작 가능한 작업이 없으면 현재 시간을 올려줌
             else now++;
+            
         }
-        
-        return answer/ (jobs.length) ;
+        return answer / jobs.length;
     }
 }
