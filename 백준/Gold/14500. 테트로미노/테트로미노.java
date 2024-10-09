fb79b3442a054b538cfc78023dfe880f17a93b55
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-10-09 21:43:29 +0900

    [Gold IV] Title: 테트로미노, Time: 1164 ms, Memory: 33116 KB -BaekjoonHub

---
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] paper;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean[][] visited;
+    static int max = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        paper = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dfs(j, i, 0, 0);
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    public static void dfs(int x, int y, int sum, int d) {
+        if (d == 4) {
+            max = Math.max(max, sum);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= M || ny < 0 || ny >= N || visited[ny][nx]) continue;
+
+            visited[ny][nx] = true;
+            dfs(nx, ny, sum + paper[ny][nx], d + 1);
+            if (d == 2)
+                dfs(x, y, sum + paper[ny][nx], d + 1);
+            visited[ny][nx] = false;
+        }
+    }
+}
