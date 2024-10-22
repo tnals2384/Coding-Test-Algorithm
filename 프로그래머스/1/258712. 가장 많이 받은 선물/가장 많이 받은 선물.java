8f538e057e5d327029de3063be11fce0e2241129
suuumim <83461362+tnals2384@users.noreply.github.com>
2024-10-22 22:41:03 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 20.45 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,47 +1,44 @@
 import java.util.*;
 class Solution {
+    //A는 선물을 준 친구 B는 선물 받은 친구
+    //선물지수 : 준 선물 - 받은 선물 
+    //더많은 선물을 준사람이 하나 받음
+    // 선물 주고받은 적 없거나 같은 수로 주고받았으면 선물지수가 큰사람이 선물 지수가 작은 사람에게 하나 받음
+    
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-        int N = friends.length;
+        int max = 0;
         
-        //인덱스 번호 매핑
-        HashMap<String, Integer> index = new HashMap<>();
-        
-        int num=0;
-        for(String f : friends) {
-            index.put(f, num++);
+        HashMap<String, Integer> name = new HashMap<>();
+        for(int i = 0; i < friends.length; i++) {
+            name.put(friends[i], i);
         }
         
-        //선물 지수와 선물 그래프 계산
-        int[] score = new int[N];
-        int[][] graph = new int[N][N];
-        for(String g : gifts) {
-            String[] friend = g.split(" ");
-            score[index.get(friend[0])]++;
-            score[index.get(friend[1])]--;
-            graph[index.get(friend[0])][index.get(friend[1])]++;
+        int[][] board = new int[friends.length][friends.length];
+        int[] value = new int[friends.length];
+        
+        for(int i = 0; i < gifts.length; i++) {
+            String[] s = gifts[i].split(" ");
+            board[name.get(s[0])][name.get(s[1])]++;
+            value[name.get(s[0])]++;
+            value[name.get(s[1])]--;
         }
         
-        //판단 시작
-        for(int i=0; i<N; i++) {
-            int count=0;
-            for(int j=0; j<N; j++) {
-                if(i==j) continue;
+        for(int i = 0; i < friends.length; i++) {
+            int count = 0;
+            for(int j = 0; j < friends.length; j++) {
+                if(i == j) continue;
                 
-                //i가 더많이 줬을 경우
-                if(graph[i][j] > graph[j][i]) {
+                if(board[i][j] > board[j][i]) {
                     count++;
                 }
-                
-                //i의 선물지수가 더 높을 경우
-                else if(graph[i][j]==graph[j][i]) {
-                    if(score[i] > score[j])
-                        count++;
+                else if(board[i][j] == board[j][i] && value[i] > value[j]) {
+                    count++;
                 }
             }
-            answer = Math.max(count, answer);
+            
+            max = Math.max(max, count);
         }
         
-        return answer;
+        return max;
     }
 }
